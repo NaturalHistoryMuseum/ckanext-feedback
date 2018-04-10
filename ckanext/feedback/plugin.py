f48c8f7af0d8ace353e52b2d72935e5447bfa3c3
Alice Butcher <alycejenni@gmail.com>
2018-04-10 13:38:37 +0100

    automated corrections

--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -1,16 +1,22 @@
-"""
-CKAN Contact Extension
-"""
+
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-feedback
+# Created by the Natural History Museum in London, UK
 
 import ckan.plugins as p
 
 class FeedbackPlugin(p.SingletonPlugin):
-    """
-    CKAN Contact Extension
-    """
+    '''CKAN Contact Extension'''
     p.implements(p.IConfigurer)
 
     ## IConfigurer
     def update_config(self, config):
-        p.toolkit.add_template_directory(config, 'theme/templates')
-        p.toolkit.add_resource('theme/public', 'ckanext-feedback')
+        '''
+
+        :param config: 
+
+        '''
+        p.toolkit.add_template_directory(config, u'theme/templates')
+        p.toolkit.add_resource(u'theme/public', u'ckanext-feedback')
--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-feedback
+# Created by the Natural History Museum in London, UK
+
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = u'0.1'
 
 setup(
-	name='ckanext-feedback',
+	name=u'ckanext-feedback',
 	version=version,
-	description='CKAN Extension providing feedback form',
+	description=u'CKAN Extension providing feedback form',
 	classifiers=[],
-	keywords='',
-	author='Ben Scott',
-	author_email='ben@benscott.co.uk',
-	url='',
-	license='',
-    packages=find_packages(exclude=['tests']),
-    namespace_packages=['ckanext', 'ckanext.feedback'],
+	keywords=u'',
+	author=u'Ben Scott',
+	author_email=u'ben@benscott.co.uk',
+	url=u'',
+	license=u'',
+    packages=find_packages(exclude=[u'tests']),
+    namespace_packages=[u'ckanext', u'ckanext.feedback'],
 	include_package_data=True,
 	zip_safe=False,
 	install_requires=[],
 	entry_points=\
-	"""
+	u'''
     [ckan.plugins]
     	feedback=ckanext.feedback.plugin:FeedbackPlugin
-	""",
+	''',
 )
